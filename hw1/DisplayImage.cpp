af33d1189d4b000c308c48b70247015602a3eb90
tomsan <tomsangotw@gmail.com>
2019-03-27 11:41:07 +0800

    hw1_complete

--- hw1/DisplayImage.cpp
+++ hw1/DisplayImage.cpp
@@ -4,6 +4,89 @@
 using namespace std;
 using namespace cv;
 
+Mat image; //Mat->Matrix 宣告一個圖片變數
+Mat result1;
+Mat cvResult1;
+Mat result2;
+Mat cvResult2;
+
+void mySharpen(){
+	for(int i = 0; i < image.rows; i++){
+		for(int j = 0; j < image.cols; j++){
+			if(i == 0 || i == image.rows - 1 || j == 0 || j == image.cols - 1){
+				result1.at<Vec3b>(i, j)[0] = 0;
+				result1.at<Vec3b>(i, j)[1] = 0;
+				result1.at<Vec3b>(i, j)[2] = 0;
+			}else{
+				int temp;
+				for(int k = 0; k < 3; k++){
+					temp = 5 * int(image.at<Vec3b>(i, j)[k]);
+					temp -= image.at<Vec3b>(i-1, j)[k];
+					temp -= image.at<Vec3b>(i, j-1)[k];
+					temp -= image.at<Vec3b>(i+1, j)[k];
+					temp -= image.at<Vec3b>(i, j+1)[k];
+					if(temp < 0){
+						result1.at<Vec3b>(i, j)[k] = 0;
+					}else if(temp > 255){
+						result1.at<Vec3b>(i, j)[k] = 255;
+					}else{
+						result1.at<Vec3b>(i, j)[k] = temp;
+					}
+				}
+			}
+			//cout << image.at<Vec3b>(i, j);
+		}
+		//cout << endl;
+	}
+}
+
+void cvSharpen(){
+	Mat kernel(3, 3, CV_32F, Scalar(0));
+	kernel.at<float>(1, 1) = 5.0;
+	kernel.at<float>(1, 0) = -1.0;
+	kernel.at<float>(0, 1) = -1.0;
+	kernel.at<float>(1, 2) = -1.0;
+	kernel.at<float>(2, 1) = -1.0;
+	filter2D(image, cvResult1, image.depth(), kernel);
+}
+
+void myGaussianBlur(){
+	for(int i = 0; i < image.rows; i++){
+		for(int j = 0; j < image.cols; j++){
+			if(i == 0 || i == image.rows - 1 || j == 0 || j == image.cols - 1){
+				result2.at<Vec3b>(i, j)[0] = 0;
+				result2.at<Vec3b>(i, j)[1] = 0;
+				result2.at<Vec3b>(i, j)[2] = 0;
+			}else{
+				int temp;
+				for(int k = 0; k < 3; k++){
+					temp = image.at<Vec3b>(i-1, j-1)[k];
+					temp += image.at<Vec3b>(i-1, j+1)[k];
+					temp += image.at<Vec3b>(i+1, j-1)[k];
+					temp += image.at<Vec3b>(i+1, j+1)[k];
+
+					temp += 2 * image.at<Vec3b>(i-1, j)[k];
+					temp += 2 * image.at<Vec3b>(i, j-1)[k];
+					temp += 2 * image.at<Vec3b>(i+1, j)[k];
+					temp += 2 * image.at<Vec3b>(i, j+1)[k];
+					
+					temp += 4 * image.at<Vec3b>(i, j)[k];
+					temp /= 16;	
+					
+					if(temp < 0){
+						result2.at<Vec3b>(i, j)[k] = 0;
+					}else if(temp > 255){
+						result2.at<Vec3b>(i, j)[k] = 255;
+					}else{
+						result2.at<Vec3b>(i, j)[k] = temp;
+					}
+				}
+			}
+		}
+	}
+}
+
+
 int main(int argc, char** argv )
 {
 	if ( argc != 2 )
@@ -11,7 +94,7 @@ int main(int argc, char** argv )
 		printf("usage: DisplayImage.out <Image_Path>\n");
 		return -1;
 	}
-	Mat image; //Mat->Matrix 宣告一個圖片變數 
+
 	image = imread( argv[1], 1 );//讀圖
 								//(檔案名稱, flag < 0原圖; flag=0 灰階; flag>0 BGR) 
 	if ( !image.data )
@@ -23,17 +106,39 @@ int main(int argc, char** argv )
 	cout << image.rows << endl;
 	cout << image.cols << endl;
 	cout << image.size() << endl;
+	cout << image.depth() << endl;//0~4
+	cout << image.type() << endl;
+	cout << image.channels() << endl;
 
-	image.at<Vec3b>(256, 256)[0] = 0;
-	image.at<Vec3b>(256, 256)[1] = 0;
-	image.at<Vec3b>(256, 256)[2] = 0;
-//	for(int i = 0; i < ; i++){
-//	
-//	}
+/*	for(int i = 0; i < image.rows; i++){
+		for(int j = 0; j < image.cols; j++){
+			cout << image.at<Vec3b>(i, j);
+		}
+		cout << endl;
+	}*/
 
+	result1.create(512, 512, CV_8UC3);
+	cvResult1.create(512, 512, CV_8UC3);
+	result2.create(512, 512, CV_8UC3);
+	cvResult2.create(512, 512, CV_8UC3);
+
+	mySharpen();
+	cvSharpen();
+	myGaussianBlur();
+	GaussianBlur(image, cvResult2, Size(3, 3), 0, 0);//後兩個, x 與 y 方向標準差	
 
 	namedWindow("Display Image", WINDOW_AUTOSIZE ); //創建一個 window 並給名稱
+	namedWindow("Sharpen Image", WINDOW_AUTOSIZE ); 
+	namedWindow("cvSharp Image", WINDOW_AUTOSIZE ); 
+	namedWindow("Blur Image", WINDOW_AUTOSIZE ); 
+	namedWindow("cvBlur Image", WINDOW_AUTOSIZE ); 
+
 	imshow("Display Image", image); //給定一個 window，並給要顯示的圖片
+	imshow("Sharpen Image", result1);
+	imshow("cvSharp Image", cvResult1);
+	imshow("Blur Image", result2);
+	imshow("cvBlur Image", cvResult2);
+
 	waitKey(0);
 	return 0;
 }
--- hw1/cmake_install.cmake
+++ hw1/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/tom/code
+# Install script for directory: /home/tom/cv/hw1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/tom/code/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/tom/cv/hw1/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
