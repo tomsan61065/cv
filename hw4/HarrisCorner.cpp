05000b5313bab306432e1f35498a68dcbfb84b67
tomsan61065 <tomsangotw@gmail.com>
2019-05-16 04:35:26 +0800

    sobel complete

--- hw4/HarrisCorner.cpp
+++ hw4/HarrisCorner.cpp
@@ -9,16 +9,50 @@ Mat image; //Mat->Matrix 宣告一個圖片變數
 
 Mat image2;
 
+void Sobel(Mat &src, Mat &resultX, Mat &resultY){
+	//use sobel 3*3 return Dx, Dy
+	resultX.create(src.size(), CV_32F);
+	resultY.create(src.size(), CV_32F);
+	for(int i = 0; i < src.rows; i++){
+		for(int j = 0; j < src.cols; j++){
+			if(i == 0 || j == 0 || i == src.rows -1 || j == src.cols -1){
+				resultX.at<float>(i, j) = 0.0;
+				resultY.at<float>(i, j) = 0.0;
+			}else{
+				float tempX = src.at<uchar>(i-1, j+1) + 2*src.at<uchar>(i, j+1) + src.at<uchar>(i+1, j+1);
+				tempX = tempX - src.at<uchar>(i-1, j-1) - 2*src.at<uchar>(i, j-1) - src.at<uchar>(i+1, j-1);
 
+				float tempY = src.at<uchar>(i+1, j-1) + 2*src.at<uchar>(i+1, j) + src.at<uchar>(i+1, j+1);
+				tempY = tempY - src.at<uchar>(i-1, j-1) - 2*src.at<uchar>(i-1, j) - src.at<uchar>(i-1, j+1);
+
+				resultX.at<float>(i, j) = tempX;
+				resultY.at<float>(i, j) = tempY;
+			//	dst.at<float>(i, j) = abs(tempX) + abs(tempY);
+			//	dst.at<float>(i, j) = sqrt(tempX*tempX + tempY*tempY);
+			}
+		}
+	}
+}
+
+void HarrisCorner(Mat &src, Mat &dst, int blockSize, int ksize, double k){
+	dst.create(src.size(), src.type());
+	Mat Dx, Dy;
+	Sobel(src, Dx, Dy);
+
+	for(int i = 0; i < src.rows; i++){
+		for(int j = 0; j < src.cols; j++){
+
+		}
+	}
+}
 
 int main(int argc, char** argv )
 {
-	image = imread( "Fig0424(a)(rectangle).tif", 0);//讀圖
+	image = imread( "test.jpg", 0);//讀圖
 								//(檔案名稱, flag < 0原圖; flag=0 灰階; flag>0 BGR) 
-	//image2 = imread("Fig0427(a)(woman).tif", 0);
 
 	//變成一樣大小
-	resize(image, image, Size(), 0.5, 0.5); //(int , out, size, fx, fy, interpolation 方法)
+	//resize(image, image, Size(), 0.5, 0.5); //(int , out, size, fx, fy, interpolation 方法)
 	//resize(image, image, Size(image.rows*0.5, image.cols*0.5), 0, 0)
 
 	cout << "cols:" << image.cols << endl;
@@ -28,25 +62,17 @@ int main(int argc, char** argv )
 	cout << "type: " << image.type() << endl;
 	cout << "channels:" << image.channels() << endl;
 
-	cout << endl << "Image2:" << endl;
-	cout << "cols:" << image2.cols << endl;
-	cout << "rows:" << image2.rows << endl;
-	cout << image2.size() << endl;
-	cout << "depth: " << image2.depth() << endl;//0~4
-	cout << "type: " << image2.type() << endl;
-	cout << "channels:" << image2.channels() << endl;
-	
-	
+
 	namedWindow("Display Image", WINDOW_AUTOSIZE);
 	imshow("Display Image", image);
 
-	namedWindow("Display Image2", WINDOW_AUTOSIZE);
-	imshow("Display Image2", image2);
-	Mat result;
-	Mat result2;
-	exchangePhaseAndMagnitude(image, image2, result, result2);
-	imshow("result e", result);
-	imshow("result f", result2);
+	Mat tempX, tempY;
+	/*
+	Sobel(image, tempX, tempY);
+	tempX.convertTo(tempX, CV_8U);
+	tempY.convertTo(tempY, CV_8U);
+	imshow("X", tempX);
+	imshow("Y", tempY);*/
 
 	waitKey(0);
 	return 0;
